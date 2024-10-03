1d84a6bbdcb6777cf9fe6d8caaf87648ca1e87fa
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-03 22:59:12 +0545

    feat: data add in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import  { FaFileAlt } from "react-icons/fa";
+import React from "react";
+import { FaFileAlt } from "react-icons/fa";
+import { AiOutlineDownload } from "react-icons/ai";
 
 function Card() {
   return (
-    <>
-      <div>
+    <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+      <FaFileAlt size="2em" />
 
-       <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
-       <FaFileAlt />
-       <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam libero dolores veniam.</p>
-       <div className='footer absolute bottom-0 bg-sky-200 w-full h-20 left-0'></div>
+      <p className="text-sm leading-tight mt-5 font-semibold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam libero
+        dolores veniam.
+      </p>
 
-       </div>
+      <div className=" footer absolute bottom-0 w-full left-0">
 
+        <div className="flex items-center justify-between px-8 py-3 mb-3">
+          <h5>.4mb</h5>
+          <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
+            <AiOutlineDownload size=".7em" color="#fff" />
+          </span>
+        </div>
+
+        <div className="tag w-full py-4 bg-green-600 flex items-center  justify-center">
+          <h3 className="text-sm font-semibold"> Download Now </h3>
+        </div>
+
+      </div>
+      
     </div>
-
-    </>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 function Foreground() {
+  const data = [
+    {
+      decs: "hlo my name is sagar iam from nepal",
+      filesize: ".9mb",
+      close: "true",
+      tag: { isOpen: true, tagTitle: "Download now", tagColor: "green" },
+    },
+  ];
+
   return (
-    
     <>
-    {/* giving z-index so it will come up and we can't select the background text */}
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-    <Card/>
-    </div>
+      {/* giving z-index so it will come up and we can't select the background text */}
+      <div className="fixed top-0 left-0 z-[3] w-full h-full">
+        <Card />
+      </div>
     </>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
