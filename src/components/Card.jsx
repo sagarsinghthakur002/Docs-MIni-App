567087d738736ccc26f553abe5f79c185a87d77a
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-04 18:51:33 +0545

    feat: adding frame motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,37 +2,37 @@ import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 import { AiOutlineDownload } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-function Card({data}) {
+function Card({ data, reference }) {
   return (
-    <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+    <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
       <FaFileAlt size="2em" />
 
       <p className="text-sm leading-tight mt-5 font-semibold">
-        {data.dec}
+        {data.decs}
       </p>
 
-      <div className=" footer absolute bottom-0 w-full left-0">
-
+      <div className="footer absolute bottom-0 w-full left-0">
         <div className="flex items-center justify-between px-8 py-3 mb-3">
           <h5>{data.filesize}</h5>
+
           <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
-            {data.close ? <IoClose /> :  <AiOutlineDownload size=".7em" color="#fff" /> }
-            {/* we pass true or pass . if close is true then show close icon or ya show download icon  */}
+            {data.close ? <IoClose /> : <AiOutlineDownload size=".7em" color="#fff" />}
           </span>
         </div>
 
-
-      {
-        data.tag.isOpen && (
-        <div className="tag w-full py-4 bg-green-600 flex items-center  justify-center">
-          <h3 className="text-sm font-semibold"> Download Now </h3>
-        </div>
-      )}
-
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-4 ${
+              data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+            } flex items-center justify-center`}
+          >
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}
       </div>
-
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useRef} from "react";
 import Card from "./Card";
 
+
 function Foreground() {
+
+  const ref = useRef(null);
+
+
   const data = [
     {
-      dec: "hlo my name is sagar iam from nepal",
+      decs: "hlo my name is sagar and i am from nepal",
       filesize: ".9mb",
+      close: "false", 
+      tag: { isOpen: false, tagTitle: "Download now", tagColor: "green" },
+    },
+    {
+      decs: "hlo my name is sagar and i am from nepal",
+      filesize: ".5mb",
       close: "true", 
-      tag: { isOpen: true, tagTitle: "Download now", tagColor: "green" },
+      tag: { isOpen: true, tagTitle: "Download now", tagColor: "blue" },
+    },
+    {
+      decs: "hlo my name is sagar and i am from nepal",
+      filesize: ".10mb",
+      close: "true", 
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
     },
   ];
 
   return (
     <>
       {/* giving z-index so it will come up and we can't select the background text */}
-      <div className="fixed top-0 left-0 z-[3] w-full h-full">
+      <  div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
         {data.map((item, index) =>(
-          <Card data={item}/>
-
-        ))}
+          <Card data={item} reference={ref}/>
+      ))}
       </div>
     </>
   );
