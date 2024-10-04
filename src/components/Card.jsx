465bbdbec28de6dda99f6283113001a9af5ba8a5
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-04 16:37:12 +0545

    feat: adding data on card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 import { AiOutlineDownload } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 
-function Card() {
+function Card({data}) {
   return (
     <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
       <FaFileAlt size="2em" />
 
       <p className="text-sm leading-tight mt-5 font-semibold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam libero
-        dolores veniam.
+        {data.dec}
       </p>
 
       <div className=" footer absolute bottom-0 w-full left-0">
 
         <div className="flex items-center justify-between px-8 py-3 mb-3">
-          <h5>.4mb</h5>
+          <h5>{data.filesize}</h5>
           <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
-            <AiOutlineDownload size=".7em" color="#fff" />
+            {data.close ? <IoClose /> :  <AiOutlineDownload size=".7em" color="#fff" /> }
+            {/* we pass true or pass . if close is true then show close icon or ya show download icon  */}
           </span>
         </div>
 
+
+      {
+        data.tag.isOpen && (
         <div className="tag w-full py-4 bg-green-600 flex items-center  justify-center">
           <h3 className="text-sm font-semibold"> Download Now </h3>
         </div>
+      )}
 
       </div>
-      
+
     </div>
   );
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -4,9 +4,9 @@ import Card from "./Card";
 function Foreground() {
   const data = [
     {
-      decs: "hlo my name is sagar iam from nepal",
+      dec: "hlo my name is sagar iam from nepal",
       filesize: ".9mb",
-      close: "true",
+      close: "true", 
       tag: { isOpen: true, tagTitle: "Download now", tagColor: "green" },
     },
   ];
@@ -15,7 +15,10 @@ function Foreground() {
     <>
       {/* giving z-index so it will come up and we can't select the background text */}
       <div className="fixed top-0 left-0 z-[3] w-full h-full">
-        <Card />
+        {data.map((item, index) =>(
+          <Card data={item}/>
+
+        ))}
       </div>
     </>
   );
